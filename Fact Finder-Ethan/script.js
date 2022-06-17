412e882d79e1a0dfdb6b6250a21c8e6239e6f80c
EthanLe1210 <107719309+EthanLe1210@users.noreply.github.com>
2022-06-17 12:58:51 -0700

    uploaded factfinder for the first time

---
+++ Fact Finder-Ethan/script.js
@@ -0,0 +1,96 @@
+//function makeSubsAqua(){
+	//var subs = document.getElementsByTagName('h3');
+	
+//	for(var i = 0; i < subs.length; i++){
+	//	subs[i].style.color = "#00FFFF";
+//	}//for
+//}//makeSubsAqua
+
+
+
+
+function showFactOne(){
+	document.getElementById("factdisplay").innerHTML = "<em>The sun'slight takes about 8 minutes to reach the Earth after it has been emitted from the sun's surface. The time it takes for light to reach planets in our Solar System (not the Milky Way, which is our galaxy) varies from about 3 minutes for Mercury, to about 5.3 hours for Pluto</em>. <img src='images/sun.jpg' id='sun'>";
+	
+	document.querySelector('body').style.backgroundColor = "#ADB6C4";
+	document.querySelector('#factdisplay').style.backgroundColor = "#FCAA67";
+	document.querySelector('form').style.backgroundColor = "#001B2E";
+	document.querySelector('#factdisplay').style.borderColor = "#690500";
+	document.querySelector('#f1').style.backgroundColor = "#343A46";
+	document.querySelector('img').style.borderColor = "#C5D86D";
+	document.querySelector('#f1').style.borderColor = "#FF2147";
+	document.querySelector('form').style.borderColor = "#B80900";
+	document.querySelector('main').style.borderColor = "#0A0B0A";
+	
+	const element = document.querySelector('h1');
+	element.style.color = "#F13E3E";
+	
+}
+
+function showFactTwo(){
+	document.getElementById("factdisplay").innerHTML = "<em>It is a commonly known fact that astronauts living aboard the International Space Station grow up to 3 percent taller while living in microgravity. They return to their normal height when backon Earth.</em>";
+	
+	document.querySelector('body').style.backgroundColor = "#F1FFE7";
+	document.querySelector('#factdisplay').style.backgroundColor = "#C2E7DA";
+	document.querySelector('form').style.backgroundColor = "#3D4452";
+	document.querySelector('#factdisplay').style.borderColor = "#959FB1";
+	document.querySelector('#f2').style.backgroundColor = "#003052";
+	document.querySelector('#f2').style.borderColor = "#272B26";
+	document.querySelector('form').style.borderColor = "#0A0B0A";
+	document.querySelector('main').style.borderColor = "#FBEA89";
+	
+	const element = document.querySelector('h1');
+	element.style.color = "#605F5E";
+	
+}
+
+function showFactThree(){
+	document.getElementById("factdisplay").innerHTML = "<em>Crabs that have lost legscan regenerate them over time. The leg breaks off at a special joint. Before molting, a new limb bud, with all the remaining leg segments, grows out of the joint. After molting, the new leg is smaller than the others.</em>";
+
+	document.querySelector('body').style.backgroundColor = "#815355";
+	document.querySelector('#factdisplay').style.backgroundColor = "#C3B299";
+	document.querySelector('form').style.backgroundColor = "#A40E4C";
+	document.querySelector('#factdisplay').style.borderColor = "#CBD4C2";
+	document.querySelector('#f3').style.backgroundColor = "#556F44";
+	document.querySelector('#f3').style.borderColor = "#E83D49";
+	document.querySelector('form').style.borderColor = "#0A0B0A";
+	document.querySelector('main').style.borderColor = "#333C2A";
+	
+	const element = document.querySelector('h1');
+	element.style.color = "#FB3640";
+
+}
+
+function showFactFour(){
+	document.getElementById("factdisplay").innerHTML = "<em>According to the Guinness World Records, the eldest person in the world was Jeanne Louise Calment, a Frenchwoman (1875- 1997) who died when she was 122.</em>";
+	
+	document.querySelector('body').style.backgroundColor = "#59A5D8";
+	document.querySelector('#factdisplay').style.backgroundColor = "#84D2F6";
+	document.querySelector('form').style.backgroundColor = "#84D2F6";
+	document.querySelector('#factdisplay').style.borderColor = "#91E5F6";
+	document.querySelector('#f4').style.backgroundColor = "#386FA4";
+	document.querySelector('#f4').style.borderColor = "#C3B299";
+	document.querySelector('form').style.borderColor = "#133C55";
+	document.querySelector('main').style.borderColor = "#123B54";
+	
+		const element = document.querySelector('h1');
+	element.style.color = "#247BA0";
+	
+}
+
+function showFactFive(){
+	document.getElementById("factdisplay").innerHTML = "<em>Buford is the only city with one resident. 60 year-old Don Sammons has been all alone in the southern Wyoming town on Interstate 80 since his son moved out over three years ago, according to Denvers 9 News. Founded in 1866, Buford at one time hosted a population of 2,000 — mostly railroad workers.</em>";
+	
+	document.querySelector('body').style.backgroundColor = "#7D451B";
+	document.querySelector('#factdisplay').style.backgroundColor = "#B3A394";
+	document.querySelector('form').style.backgroundColor = "#F6F740";
+	document.querySelector('#factdisplay').style.borderColor = "#373F51";
+	document.querySelector('#f5').style.backgroundColor = "#813405";
+	document.querySelector('#f5').style.borderColor = "#373F51";
+	document.querySelector('form').style.borderColor = "#373F51";
+	document.querySelector('main').style.borderColor = "#6D7B9C";
+	
+		const element = document.querySelector('h1');
+	element.style.color = "#A77464";
+	
+}
